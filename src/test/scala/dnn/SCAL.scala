f44b77252d61a596d8d35fb92cbd9b912f464ecb
nvedula <nvedula@sfu.ca>
2019-08-27 14:57:42 -0700

    adding dnn_layers directory

--- src/test/scala/dnn/SCAL.scala
+++ src/test/scala/dnn/SCAL.scala
@@ -32,11 +32,13 @@ class FXSCALCompTests(df: SCALNode[FXmatNxN])
   poke(df.io.enable.valid, true)
   poke(df.io.enable.bits.control, true)
   // 0x32 0011.0010 . Fixed point 3.125 in fixed point 4 BP.
-  poke(df.io.LeftIO.bits.data, 0x32323232L)
+//  poke(df.io.LeftIO.bits.data, 0x32323232L)
+  poke(df.io.LeftIO.bits.data, 0x32L)
   poke(df.io.LeftIO.valid, true)
   poke(df.io.LeftIO.bits.predicate, true)
 
   // 0x32 (3.125) * 0x20 (2.0) = 6.25 (0x64 or 100)
+//  poke(df.io.RightIO.bits.data, 0x20L)
   poke(df.io.RightIO.bits.data, 0x20L)
   poke(df.io.RightIO.valid, true)
   poke(df.io.RightIO.bits.predicate, true)
@@ -73,7 +75,7 @@ class SCALCompTester extends FlatSpec with Matchers {
     //    } should be(true)
 
     chisel3.iotesters.Driver.execute(Array("--backend-name", "verilator", "--target-dir", "test_run_dir"),
-      () => new SCALNode(NumOuts = 1, ID = 0, 4, "add")(new FXmatNxN(2, 4))) {
+      () => new SCALNode(NumOuts = 1, ID = 0, 4, "mul")(new FXmatNxN(2, 4))) {
       c => new FXSCALCompTests(c)
     } should be(true)
     //    chisel3.iotesters.Driver.execute(Array("--backend-name", "verilator", "--target-dir", "test_run_dir"),
---
+++ src/test/scala/dnn_layers/Mac.scala
@@ -0,0 +1,42 @@
+package dnn_layers
+
+import chisel3._
+import chisel3.iotesters.PeekPokeTester
+import config._
+import node._
+import org.scalatest.{FlatSpec, Matchers}
+import FPU._
+
+// Tester.
+
+class FXSCALCompTests(df: Mac[FXmatNxN])
+                     (implicit p: config.Parameters) extends PeekPokeTester(df) {
+  poke(df.io.enable.valid, true)
+  poke(df.io.enable.bits.control, true)
+  // 0x32 0011.0010 . Fixed point 3.125 in fixed point 4 BP.
+  //  poke(df.io.LeftIO.bits.data, 0x32323232L)
+  poke(df.io.LeftIO.bits.data, 0x32L)
+  poke(df.io.LeftIO.valid, true)
+  poke(df.io.LeftIO.bits.predicate, true)
+
+  // 0x32 (3.125) * 0x20 (2.0) = 6.25 (0x64 or 100)
+  //  poke(df.io.RightIO.bits.data, 0x20L)
+  poke(df.io.RightIO.bits.data, 0x20L)
+  poke(df.io.RightIO.valid, true)
+  poke(df.io.RightIO.bits.predicate, true)
+
+  poke(df.io.Out(0).ready, true.B)
+  step(20)
+}
+
+
+
+class MacCompTester extends FlatSpec with Matchers {
+  implicit val p = config.Parameters.root((new Mat_VecConfig).toInstance)
+  it should "Typ Compute Tester" in {
+    chisel3.iotesters.Driver.execute(Array("--backend-name", "verilator", "--target-dir", "test_run_dir"),
+      () => new Mac(NumOuts = 1, ID = 0, 4, "mul")(new FXmatNxN(2, 4))) {
+      c => new FXSCALCompTests(c)
+    } should be(true)
+  }
+}
