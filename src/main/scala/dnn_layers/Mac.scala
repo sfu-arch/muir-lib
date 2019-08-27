f2ffe08583457b9e6bd4f05db97c9a7e23b8c209
nvedula <nvedula@sfu.ca>
2019-08-27 16:57:18 -0700

    using DotNode in dnn_layers.mac

--- src/main/scala/dnn_layers/Mac.scala
+++ src/main/scala/dnn_layers/Mac.scala
@@ -1,5 +1,6 @@
 package dnn_layers
 
+import dnn.{DotIO, DotNode}
 import chisel3._
 import chisel3.util.{Decoupled, Enum, Valid}
 import chisel3.{Bundle, Flipped, Module, Output, RegInit, UInt, assert, printf, when}
@@ -8,40 +9,55 @@ import config._
 import dnn.SatCounterModule
 import dnn.types.OperatorDot
 import interfaces.CustomDataBundle
+import node.FXmatNxN
 //import javafx.scene.chart.PieChart.Data
 import node.{AluGenerator, HandShakingIONPS, HandShakingNPS, Shapes}
 
-class DotFU[gen <: Shapes : OperatorDot](left: => gen, lanes: Int, opCode: String)(implicit val p: Parameters) extends Module {
-  val io = IO(new Bundle {
-    val a = Flipped(Valid(left))
-    val b = Flipped(Valid(left))
-    val o = Output(Valid(left))
-  })
+//class DotFU[gen <: Shapes : OperatorDot](left: => gen, lanes: Int, opCode: String)(implicit val p: Parameters) extends Module {
+//  val io = IO(new Bundle {
+//    val a = Flipped(Valid(left))
+//    val b = Flipped(Valid(left))
+//    val o = Output(Valid(left))
+//  })
+//
+//
+//  val start = io.a.valid && io.b.valid
+//  val FU    = OperatorDot.magic(io.a.bits, io.b.bits, start, lanes, opCode)
+//  io.o.bits := FU._1
+//  val latency = FU._2
+//  val latCnt  = Module(new SatCounterModule(latency))
+//  latCnt.io.start := start
+//  io.o.valid := latCnt.io.wrap
+//}
 
-
-  val start = io.a.valid && io.b.valid
-  val FU    = OperatorDot.magic(io.a.bits, io.b.bits, start, lanes, opCode)
-  io.o.bits := FU._1
-  val latency = FU._2
-  val latCnt  = Module(new SatCounterModule(latency))
-  latCnt.io.start := start
-  io.o.valid := latCnt.io.wrap
-}
-
-class DotIO[gen <: Shapes](NumOuts: Int)(left: => gen)(implicit p: Parameters)
-  extends HandShakingIONPS(NumOuts)(new CustomDataBundle(UInt((left.getWidth).W))) {
-  // LeftIO: Left input data for computation
-  val LeftIO = Flipped(Decoupled(new CustomDataBundle(UInt((left.getWidth).W))))
-
-  // RightIO: Right input data for computation
-  val RightIO = Flipped(Decoupled(new CustomDataBundle(UInt((left.getWidth).W))))
-
-  override def cloneType = new DotIO(NumOuts)(left).asInstanceOf[this.type]
-}
+//class DotIO[gen <: Shapes](NumOuts: Int)(left: => gen)(implicit p: Parameters)
+//  extends HandShakingIONPS(NumOuts)(new CustomDataBundle(UInt((left.getWidth).W))) {
+//  // LeftIO: Left input data for computation
+//  val LeftIO = Flipped(Decoupled(new CustomDataBundle(UInt((left.getWidth).W))))
+//
+//  // RightIO: Right input data for computation
+//  val RightIO = Flipped(Decoupled(new CustomDataBundle(UInt((left.getWidth).W))))
+//
+//  override def cloneType = new DotIO(NumOuts)(left).asInstanceOf[this.type]
+//}
 
 class Mac[L <: Shapes : OperatorDot](NumOuts: Int, ID: Int, lanes: Int, opCode: String)(left: => L)(implicit p: Parameters)
   extends HandShakingNPS(NumOuts, ID)(new CustomDataBundle(UInt(left.getWidth.W)))(p) {
   override lazy val io = IO(new DotIO(NumOuts)(left))
+
+//  val dotio =  IO(Flipped(new DotIO(NumOuts)(left)))
+
+  val dotNode = Module(new DotNode(NumOuts = NumOuts, ID = ID, lanes, "Mul")(left))
+  val 
+  dotNode.io.enable <> io.enable
+  dotNode.io.LeftIO <> io.LeftIO
+  dotNode.io.RightIO <> io.RightIO
+
+    // Wire up Outputs
+    for (i <- 0 until NumOuts) {
+      io.Out(i)  <> dotNode.io.Out(i)
+    }
+
 
   /*===========================================*
  *            Registers                      *
@@ -64,82 +80,82 @@ class Mac[L <: Shapes : OperatorDot](NumOuts: Int, ID: Int, lanes: Int, opCode: 
 
   val predicate = left_R.predicate & right_R.predicate & IsEnable( )
   val start     = left_R.valid & right_R.valid & IsEnableValid( )
-
-  /*===============================================*
-   *            Latch inputs. Wire up left       *
-   *===============================================*/
-
-  // Predicate register
-  val pred_R = RegInit(init = false.B)
-
-  //printfInfo("start: %x\n", start)
-
-  io.LeftIO.ready := ~left_R.valid
-  when(io.LeftIO.fire( )) {
-    //printfInfo("Latch left data\n")
-    left_R.data := io.LeftIO.bits.data
-    left_R.valid := true.B
-    left_R.predicate := io.LeftIO.bits.predicate
-  }
-
-  io.RightIO.ready := ~right_R.valid
-  when(io.RightIO.fire( )) {
-    //printfInfo("Latch right data\n")
-    right_R.data := io.RightIO.bits.data
-    right_R.valid := true.B
-    right_R.predicate := io.RightIO.bits.predicate
-  }
-
-  // Wire up Outputs
-  for (i <- 0 until NumOuts) {
-    io.Out(i).bits.data := data_R.data
-    io.Out(i).bits.valid := true.B
-    io.Out(i).bits.predicate := predicate
-    io.Out(i).bits.taskID := left_R.taskID | right_R.taskID | enable_R.taskID
-  }
-
-  /*============================================*
- *            ACTIONS (possibly dangerous)    *
- *============================================*/
-
-  val FU = Module(new DotFU(left, lanes, opCode))
-  FU.io.a.bits := (left_R.data).asTypeOf(left)
-  FU.io.b.bits := (right_R.data).asTypeOf(left)
-
-  data_R.predicate := predicate
-  pred_R := predicate
-  FU.io.a.valid := false.B
-  FU.io.b.valid := false.B
-  //  This is written like this to enable FUs that are dangerous in the future.
-  // If you don't start up then no value passed into function
-  when(start & state === s_idle) {
-    when(predicate) {
-      FU.io.a.valid := true.B
-      FU.io.b.valid := true.B
-      state := s_ACTIVE
-    }.otherwise {
-      state := s_COMPUTE
-      ValidOut( )
-    }
-  }
-
-  when(state === s_ACTIVE) {
-    when(FU.io.o.valid) {
-      ValidOut( )
-      data_R.data := (FU.io.o.bits).asTypeOf(UInt(left.getWidth.W))
-      data_R.valid := FU.io.o.valid
-      state := s_COMPUTE
-    }.otherwise {
-      state := s_ACTIVE
-    }
-  }
-  when(IsOutReady( ) && state === s_COMPUTE) {
-    left_R := CustomDataBundle.default(0.U((left.getWidth).W))
-    right_R := CustomDataBundle.default(0.U((left.getWidth).W))
-    data_R := CustomDataBundle.default(0.U((left.getWidth).W))
-    Reset( )
-    state := s_idle
-  }
+//
+//  /*===============================================*
+//   *            Latch inputs. Wire up left       *
+//   *===============================================*/
+//
+//  // Predicate register
+//  val pred_R = RegInit(init = false.B)
+//
+//  //printfInfo("start: %x\n", start)
+//
+//  io.LeftIO.ready := ~left_R.valid
+//  when(io.LeftIO.fire( )) {
+//    //printfInfo("Latch left data\n")
+//    left_R.data := io.LeftIO.bits.data
+//    left_R.valid := true.B
+//    left_R.predicate := io.LeftIO.bits.predicate
+//  }
+//
+//  io.RightIO.ready := ~right_R.valid
+//  when(io.RightIO.fire( )) {
+//    //printfInfo("Latch right data\n")
+//    right_R.data := io.RightIO.bits.data
+//    right_R.valid := true.B
+//    right_R.predicate := io.RightIO.bits.predicate
+//  }
+//
+//  // Wire up Outputs
+//  for (i <- 0 until NumOuts) {
+//    io.Out(i).bits.data := data_R.data
+//    io.Out(i).bits.valid := true.B
+//    io.Out(i).bits.predicate := predicate
+//    io.Out(i).bits.taskID := left_R.taskID | right_R.taskID | enable_R.taskID
+//  }
+//
+//  /*============================================*
+// *            ACTIONS (possibly dangerous)    *
+// *============================================*/
+//
+//  val FU = Module(new DotFU(left, lanes, opCode))
+//  FU.io.a.bits := (left_R.data).asTypeOf(left)
+//  FU.io.b.bits := (right_R.data).asTypeOf(left)
+//
+//  data_R.predicate := predicate
+//  pred_R := predicate
+//  FU.io.a.valid := false.B
+//  FU.io.b.valid := false.B
+//  //  This is written like this to enable FUs that are dangerous in the future.
+//  // If you don't start up then no value passed into function
+//  when(start & state === s_idle) {
+//    when(predicate) {
+//      FU.io.a.valid := true.B
+//      FU.io.b.valid := true.B
+//      state := s_ACTIVE
+//    }.otherwise {
+//      state := s_COMPUTE
+//      ValidOut( )
+//    }
+//  }
+//
+//  when(state === s_ACTIVE) {
+//    when(FU.io.o.valid) {
+//      ValidOut( )
+//      data_R.data := (FU.io.o.bits).asTypeOf(UInt(left.getWidth.W))
+//      data_R.valid := FU.io.o.valid
+//      state := s_COMPUTE
+//    }.otherwise {
+//      state := s_ACTIVE
+//    }
+//  }
+//  when(IsOutReady( ) && state === s_COMPUTE) {
+//    left_R := CustomDataBundle.default(0.U((left.getWidth).W))
+//    right_R := CustomDataBundle.default(0.U((left.getWidth).W))
+//    data_R := CustomDataBundle.default(0.U((left.getWidth).W))
+//    Reset( )
+//    state := s_idle
+//  }
 
 
   printf(p"\n State : ${state} Predicate ${predicate} Left ${left_R} Right ${right_R} Output: ${data_R}")
--- src/test/scala/dnn_layers/Mac.scala
+++ src/test/scala/dnn_layers/Mac.scala
@@ -10,24 +10,6 @@ import FPU._
 //import dnn.DotNode
 
 // Tester.
-class DotCompTests(df: Mac[matNxN])
-                  (implicit p: config.Parameters) extends PeekPokeTester(df) {
-  poke(df.io.enable.valid, true)
-  poke(df.io.enable.bits.control, true)
-
-  poke(df.io.LeftIO.bits.data, 0xFEFEFEFEL)
-  poke(df.io.LeftIO.valid, true)
-  poke(df.io.LeftIO.bits.predicate, true)
-
-
-  poke(df.io.RightIO.bits.data, 0x04020402L)
-  poke(df.io.RightIO.valid, true)
-  poke(df.io.RightIO.bits.predicate, true)
-
-  poke(df.io.Out(0).ready, true.B)
-  step(20)
-}
-
 
 class FXDotCompTests(df: Mac[FXmatNxN])
                     (implicit p: config.Parameters) extends PeekPokeTester(df) {
@@ -47,40 +29,14 @@ class FXDotCompTests(df: Mac[FXmatNxN])
   step(20)
 }
 
-class FPDotCompTests(df: Mac[FPmatNxN])
-                    (implicit p: config.Parameters) extends PeekPokeTester(df) {
-  poke(df.io.enable.valid, true)
-  poke(df.io.enable.bits.control, true)
-  // 0x49 = 3.125 (Mini 8 bit format. 3 bit exp, 5 bit mantissa
-  poke(df.io.LeftIO.bits.data, 0x49494949L)
-  poke(df.io.LeftIO.valid, true)
-  poke(df.io.LeftIO.bits.predicate, true)
-
-  // 0x4e - 3.7 . Result : 103.
-  poke(df.io.RightIO.bits.data, 0x4e4e4e4eL)
-  poke(df.io.RightIO.valid, true)
-  poke(df.io.RightIO.bits.predicate, true)
-
-  poke(df.io.Out(0).ready, true.B)
-  step(20)
-}
 
 
 class DotCompTester extends FlatSpec with Matchers {
   implicit val p = config.Parameters.root((new Mat_VecConfig).toInstance)
   it should "Typ Compute Tester" in {
-    //    chisel3.iotesters.Driver.execute(Array("--backend-name", "verilator", "--target-dir", "test_run_dir"),
-    //      () => new DotNode(NumOuts = 1, ID = 0, 4, "Add")(new matNxN(2, true))) {
-    //      c => new DotCompTests(c)
-    //    } should be(true)
-
     chisel3.iotesters.Driver.execute(Array("--backend-name", "verilator", "--target-dir", "test_run_dir"),
       () => new Mac(NumOuts = 1, ID = 0, 4, "Mul")(new FXmatNxN(2,4))) {
       c => new FXDotCompTests(c)
     } should be(true)
-    //    chisel3.iotesters.Driver.execute(Array("--backend-name", "verilator", "--target-dir", "test_run_dir"),
-    //      () => new DotNode(NumOuts = 1, ID = 0, 4, "Mul")(new FPmatNxN(2, t = FType.M))) {
-    //      c => new FPDotCompTests(c)
-    //    } should be(true)
   }
 }
