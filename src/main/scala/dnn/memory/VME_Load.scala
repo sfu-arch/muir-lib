2e60b103867548774105fb0899ab23180bf03247
Reza Hojabr <reza.hojabr@gmail.com>
2019-09-19 15:11:57 -0700

    A buffer is added to VME_load

--- src/main/scala/dnn/memory/VME_Load.scala
+++ src/main/scala/dnn/memory/VME_Load.scala
@@ -49,44 +49,85 @@ class VME_Load(debug: Boolean = false)(implicit p: Parameters) extends Module {
 
   })
 
-  val inDataCounter = Counter(math.pow(2, io.vme_cmd.bits.lenBits).toInt)
+  val ReadDataCounter = Counter(math.pow(2, io.vme_cmd.bits.lenBits).toInt)
+  val WriteDataCounter = Counter(math.pow(2, io.vme_cmd.bits.lenBits).toInt)
+  val buffer = Module(new Queue(io.vme_read.data.bits.cloneType, 50))
 
   val StoreType = Module(new TypStore(NumPredOps = 0, NumSuccOps = 0, NumOuts = 1, ID = 0, RouteID = 0))
 
   StoreType.io.enable.bits := ControlBundle.active()
   StoreType.io.enable.valid := true.B
   StoreType.io.Out(0).ready := true.B
+//  StoreType.io.GepAddr := io.base_addr
 
   io.memReq <> StoreType.io.memReq
   StoreType.io.memResp <> io.memResp
 
-  val vme_data_queue = Module(new Queue(io.vme_read.data.bits.cloneType, 50))
 
   io.vme_read.cmd <> io.vme_cmd
 
-  val sIdle :: sReadData :: sGepAddr :: Nil = Enum(3)
-  val state = RegInit(sIdle)
+  val sIdle :: sReq :: sBusy :: Nil = Enum(3)
+  val Rstate = RegInit(sIdle)
+  val Wstate = RegInit(sIdle)
+
+  // Read from VMEReadMaster
+  switch (Rstate) {
+    is (sIdle) {
+      when (io.start) {
+        ReadDataCounter.value := 0.U
+        Rstate := sReq
+      }
+    }
+    is (sReq) {
+      when (io.vme_read.cmd.fire()) {
+        Rstate := sBusy
+      }
+    }
+  }
+
+  when (Rstate =/= sIdle) {
+    ReadDataCounter.inc( )
+  }
+
+  when(Rstate === sReq) {
+    io.vme_read.cmd.valid := true.B
+  }
+
+  when(ReadDataCounter.value === io.vme_cmd.bits.len) {
+    Rstate := sIdle
+  }
+
+  buffer.io.enq <> io.vme_read.data
+  buffer.io.enq.bits := io.vme_read.data.bits + 5.U
+//  StoreType.io.inData <> buffer.io.deq
+//  StoreType.io.GepAddr := io.base_addr.data + ReadDataCounter.value
+
 
   io.done := false.B
+
+  val sWIdle :: sWriteData :: sGepAddr :: Nil = Enum(3)
+  val state = RegInit(sWIdle)
+
   switch(state) {
-    is(sIdle) {
+    is(sWIdle) {
       when(io.start && io.vme_cmd.fire()) {
         state := sGepAddr
+        WriteDataCounter.value := 0.U
       }
     }
     is(sGepAddr) {
-      when(StoreType.io.GepAddr.ready && StoreType.io.inData.ready && vme_data_queue.valid) {
-        //StoreType.io.inData.enq(vme_data_queue.deq())
-        //StoreType.io.GepAddr.enq(io.base_addr + inDataCounter.value)
-        state := sReadData
+      when(StoreType.io.GepAddr.ready && StoreType.io.inData.ready) {
+        StoreType.io.inData := buffer.io.deq
+        StoreType.io.GepAddr := io.base_addr.data + WriteDataCounter.value
+        state := sWriteData
       }
     }
-    is(sReadData) {
+    is(sWriteData) {
       when(StoreType.io.Out(0).fire) {
-        inDataCounter.inc()
+        WriteDataCounter.inc()
         state := sGepAddr
-      }.elsewhen(inDataCounter.value === io.vme_cmd.bits.len) {
-        state := sIdle
+      }.elsewhen(WriteDataCounter.value === io.vme_cmd.bits.len) {
+        state := sWIdle
         io.done := true.B
       }
     }
@@ -99,7 +140,7 @@ class VME_Load(debug: Boolean = false)(implicit p: Parameters) extends Module {
       printf("[VME_Load] start\n")
     }
     // done
-    when(state === sReadData) {
+    when(state === sWriteData) {
       when(io.done) {
         printf("[Load] Reading data\n")
       }.otherwise {
