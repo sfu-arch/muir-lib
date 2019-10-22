2c38f2c1739650b6d27d3847cf8a404ad8f937c2
Reza Hojabr <reza.hojabr@gmail.com>
2019-10-21 23:58:05 -0700

    ShapeShifter

--- src/main/scala/dnnnode/ShapeShifter.scala
+++ src/main/scala/dnnnode/ShapeShifter.scala
@@ -1,5 +1,6 @@
 package dnnnode
 
+import Chisel.Enum
 import chisel3.util.{Decoupled, Queue}
 import chisel3.{Flipped, Module, UInt, _}
 import config.{Parameters, XLEN}
@@ -7,15 +8,15 @@ import dnn.types.{OperatorDot, OperatorReduction}
 import interfaces.CustomDataBundle
 import node.{HandShakingIONPS, HandShakingNPS, Shapes, matNxN, vecN}
 
-class ShapeShifterIO[gen <: vecN , gen2 <: Shapes](NumIns: Int, NumOuts: Int)(shapeIn: => gen)(shapeOut: => gen2)(implicit p: Parameters)
-  extends HandShakingIONPS(NumOuts)(new CustomDataBundle(UInt(shapeOut.getWidth.W))) {
+class ShapeShifterIO[gen <: vecN , gen2 <: Shapes](NumIns: Int)(shapeIn: => gen)(shapeOut: => gen2)(implicit p: Parameters)
+  extends HandShakingIONPS(1)(new CustomDataBundle(UInt(shapeOut.getWidth.W))) {
     val in = Vec(NumIns, Flipped(Decoupled(new CustomDataBundle(UInt(shapeIn.getWidth.W)))))
-    override def cloneType = new ShapeShifterIO(NumIns, NumOuts)(shapeIn)(shapeOut).asInstanceOf[this.type]
+    override def cloneType = new ShapeShifterIO(NumIns)(shapeIn)(shapeOut).asInstanceOf[this.type]
   }
 
-class ShapeShifter[L <: vecN, K <: Shapes](NumIns: Int, NumOuts: Int, ID: Int, lanes: Int)(shapeIn: => L)(shapeOut: => K)(implicit p: Parameters)
-  extends HandShakingNPS(NumOuts, ID)(new CustomDataBundle(UInt(shapeOut.getWidth.W)))(p) {
-  override lazy val io = IO(new ShapeShifterIO(NumIns, NumOuts)(shapeIn)(shapeOut))
+class ShapeShifter[L <: vecN, K <: Shapes](NumIns: Int, ID: Int, lanes: Int)(shapeIn: => L)(shapeOut: => K)(implicit p: Parameters)
+  extends HandShakingNPS(1, ID)(new CustomDataBundle(UInt(shapeOut.getWidth.W)))(p) {
+  override lazy val io = IO(new ShapeShifterIO(NumIns)(shapeIn)(shapeOut))
 
   val x = Wire(shapeIn)
   val y = x.toVecUInt()
@@ -24,15 +25,40 @@ class ShapeShifter[L <: vecN, K <: Shapes](NumIns: Int, NumOuts: Int, ID: Int, l
   val z = x.data
   val buffer = Module(new Queue(shapeOut,40))
 
+
+
+
+/*===========================================*
+ *            Registers                      *
+ *===========================================*/
   val dataIn_R = RegInit(Vec(NumIns, CustomDataBundle.default(0.U(shapeIn.getWidth.W))))
 
+  val s_idle :: s_LATCH :: s_ACTIVE :: s_COMPUTE :: Nil = Enum(4)
+  val state = RegInit(s_idle)
+
+  /*==========================================*
+   *           Predicate Evaluation           *
+   *==========================================*/
+
+  val predicate = dataIn_R(0).predicate &  IsEnable( )
+  val start     = dataIn_R(0).valid & IsEnableValid( )
+
+  /*===============================================*
+   *            Latch inputs. Wire up left       *
+   *===============================================*/
+
+  // Predicate register
+  val pred_R = RegInit(init = false.B)
+
+  //printfInfo("start: %x\n", start)
   for (i <- 0 until NumIns) {
-    dataIn_R(i) <> io.in(i)
+    io.in(i).ready := ~dataIn_R(i).valid
+    when(io.in(i).fire()) {
+      dataIn_R(i).data := io.in(i).bits.data
+      dataIn_R(i).valid := true.B
+      dataIn_R(i).predicate := io.in(i).bits.predicate
+    }
   }
-
-
-
-
 
   //  printf(p"\n Left ${io.LeftIO.bits.data} Right: ${io.RightIO.bits.data} Output: ${reduceNode.io.Out(0).bits.data}")
 }
