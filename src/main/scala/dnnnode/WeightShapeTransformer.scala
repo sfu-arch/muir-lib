19bd46c72d8b59c86f4ef58dcbfbf9c8678eab89
Reza Hojabr <reza.hojabr@gmail.com>
2019-10-29 18:11:42 -0700

    weight tranformer

--- src/main/scala/dnnnode/WeightShapeTransformer.scala
+++ src/main/scala/dnnnode/WeightShapeTransformer.scala
@@ -1,36 +1,70 @@
 package dnnnode
 
 import Chisel.Enum
+import breeze.numerics.ceil
+import chisel3._
 import chisel3.util._
-import chisel3.{Module, UInt, _}
+import chisel3.{Module, UInt}
 import config.{Parameters, XLEN}
 import dnn.memory.{TensorMaster, TensorParams}
-import interfaces._
-import node._
+import interfaces.CustomDataBundle
+import node.{vecN}
 import shell.ShellKey
+import dnn.memory.ISA._
 
-abstract class WeightShapeTransformerIO[gen <: Shapes](tensorType: String = "none")(wgtShape: => gen)(implicit val p: Parameters)
+class WeightShapeTransformerIO[gen <: vecN](tensorType: String = "none")(wgtShape: => gen)(implicit val p: Parameters)
   extends Module {
   val tp = new TensorParams(tensorType)
-  val mp = p(ShellKey).memParams
   val io = IO(new Bundle {
     val start = Input(Bool())
     val done = Output(Bool())
-    val numWeight = Input(UInt(mp.addrBits.W))
-    val xsize = Input(UInt(mp.addrBits.W))
+    val xsize = Input(UInt(M_SIZE_BITS.W))
     val tensorMaster = new TensorMaster(tensorType)
     val tensor = new TensorMaster(tensorType)
   })
 }
 
-class WeightShapeTransformer[L <: Shapes] (Kx: Int, Ky: Int, tensorType: String = "none")(wgtShape: => L)(implicit p: Parameters)
+class WeightShapeTransformer[L <: vecN] (numWeight: Int, tensorType: String = "none")(wgtShape: => L)(implicit p: Parameters)
   extends WeightShapeTransformerIO(tensorType)(wgtShape)(p) {
 
   val buffer = Module(new WeightQueue(UInt(p(XLEN).W), 144, 16, 9))
-  val readCnt = Counter(10)
+  val wgtTensorLength = ceil(numWeight * wgtShape.N / tp.tensorWidth)
+
+  val writeBufCntOn = RegInit(init = false.B)
+  val (writeBufCnt, writeWrap) = Counter(writeBufCntOn, wgtTensorLength)
+
+  val readWgtCnt = Counter(numWeight)
+
+
   val s_idle :: s_BufferWrite :: s_Transfer :: s_Finish :: Nil = Enum(4)
   val state = RegInit(s_idle)
 
-  io.tensor.wr <> DontCare
+  val tensorFile = SyncReadMem(numWeight, wgtShape)
+
+  buffer.io.enq.valid := io.tensorMaster.rd.data.valid
+  buffer.io.enq.bits := io.tensorMaster.rd.data.bits(0)
+  io.tensorMaster.rd.idx.bits := writeBufCnt
+  io.tensorMaster.rd.idx.valid := buffer.io.enq.ready
+  io.tensorMaster.wr <> DontCare
+
+  when (writeWrap) {writeBufCntOn := false.B}
+  when (io.start) {writeBufCntOn := true.B}
+
+  when (buffer.io.deq.valid & readWgtCnt.value <= numWeight.U) {
+    tensorFile.write(readWgtCnt.value, buffer.io.deq.bits.asTypeOf(wgtShape))
+    readWgtCnt.inc()
+  }
+  when (readWgtCnt.value === (numWeight - 1).U) {
+    io.done := true.B
+  }.otherwise{
+    io.done := false.B
+  }
+
+  val rvalid = RegNext(io.tensor.rd.idx.valid)
+  io.tensor.rd.data.valid := rvalid
+
+  val rdata = tensorFile.read(io.tensor.rd.idx.bits, io.tensor.rd.idx.valid)
+  io.tensor.rd.data.bits := rdata.asUInt.asTypeOf(io.tensor.rd.data.bits)
+
 }
 
