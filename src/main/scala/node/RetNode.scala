c2c0d9056597f50b5171efd4e25f6c248c822827
Amirali Sharifian <am.sharifian@gmail.com>
2020-03-31 21:14:25 -0700

    Update debug interface

--- src/main/scala/node/RetNode.scala
+++ src/main/scala/node/RetNode.scala
@@ -14,108 +14,6 @@ import muxes._
 import util._
 import dandelion.config._
 
-
-/*
-class RetNodeIO(val NumPredOps: Int, val retTypes: Seq[Int])(implicit p: Parameters)
-  extends Bundle {
-  val enable = Flipped(Decoupled(new ControlBundle()))
-  val PredOp = Flipped(Vec(NumPredOps, Decoupled(new ControlBundle())))
-  val In  = Flipped(new VariableDecoupledData(retTypes)) // Data to be returned
-  val Out = Decoupled(new Call(retTypes)) // Returns to calling block(s)
-}
-
-class RetNode(NumPredOps: Int = 0, retTypes: Seq[Int], ID: Int)
-             (implicit val p: Parameters,
-              name: sourcecode.Name,
-              file: sourcecode.File) extends Module
-  with CoreParams with UniformPrintfs {
-
-  val node_name = name.value
-  val module_name = file.value.split("/").tail.last.split("\\.").head.capitalize
-
-  override lazy val io = IO(new RetNodeIO(NumPredOps, retTypes)(p))
-  override val printfSigil = module_name + ": " + node_name + ID + " "
-
-
-  val (cycleCount, _) = Counter(true.B, 32 * 1024)
-
-  // Defining states
-  val s_IDLE :: s_COMPUTE :: Nil = Enum(2)
-  val state = RegInit(s_IDLE)
-
-  // Enable
-  //val enable_R = RegInit(ControlBundle.default)
-  val enable_valid_R = RegInit(false.B)
-
-  //val task_ID_R = RegNext(next = enable_R.taskID)
-
-  // Data Inputs
-  val inputReady = Seq.fill(retTypes.length + NumPredOps + 1)(RegInit(false.B))
-  val in_data_valid_R = Seq.fill(retTypes.length)(RegInit(false.B))
-
-  // Output registers
-  val outputReg = RegInit(0.U.asTypeOf(io.Out))
-  val out_ready_R = RegInit(false.B)
-  val out_valid_R = RegInit(false.B)
-
-
-  // Latching enable signal
-  io.enable.ready := ~enable_valid_R
-  when(io.enable.fire()) {
-    enable_valid_R := io.enable.valid
-    //enable_R <> io.enable.bits
-    outputReg.bits.enable := io.enable.bits
-  }
-
-  // Latching input data
-  for (i <- retTypes.indices) {
-    io.In.data(s"field$i").ready := ~in_data_valid_R(i)
-    when(io.In.data(s"field$i").fire()) {
-      outputReg.bits.data(s"field$i") <> io.In.data(s"field$i").bits
-      in_data_valid_R(i) := true.B
-    }
-  }
-
-  for (i <- (retTypes.length + 1) until (retTypes.length + 1 + NumPredOps)) {
-    io.PredOp(i - (retTypes.length + 1)).ready := inputReady(i)
-  }
-
-  when(io.Out.fire()) {
-    out_ready_R := io.Out.ready
-    out_valid_R := false.B
-  }
-
-  io.Out.bits := outputReg.bits
-  io.Out.valid := out_valid_R
-
-  switch(state) {
-    is(s_IDLE) {
-      when(enable_valid_R) {
-        when(in_data_valid_R.reduceLeft(_ && _)) {
-          out_valid_R := true.B
-          state := s_COMPUTE
-        }
-      }
-    }
-    is(s_COMPUTE) {
-      when(out_ready_R) {
-        for (i <- retTypes.indices) {
-          in_data_valid_R(i) := false.B
-        }
-
-        out_valid_R := false.B
-        enable_valid_R := false.B
-        out_ready_R := false.B
-
-        state := s_IDLE
-        printf("[LOG] " + "[" + module_name + "] " + "[TID->%d] " + node_name + ": Output fired @ %d, Value: %d\n", outputReg.bits.enable.taskID, cycleCount, outputReg.bits.data(s"field0").data)
-      }
-    }
-  }
-
-}
-*/
-
 class RetNodeIO(val retTypes: Seq[Int])(implicit p: Parameters)
   extends Bundle {
   val enable = Flipped(Decoupled(new ControlBundle()))
@@ -214,7 +112,7 @@ class RetNode(retTypes: Seq[Int], ID: Int)
 
 }
 
-class RetNode2IO(val retTypes: Seq[Int], Debug:Boolean = false , NumBores : Int = 0)(implicit p: Parameters)
+class RetNode2IO(retTypes: Seq[Int], Debug:Boolean = false , NumBores : Int = 0)(implicit val p: Parameters)
   extends Bundle {
   val In = Flipped(new CallDecoupled(retTypes))
   val Out = Decoupled(new Call(retTypes)) // Returns to calling block(s)
