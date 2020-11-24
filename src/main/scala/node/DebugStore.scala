fbf045d60d108adb52f9a1ae5afb2a9a090fe5d7
Amirali Sharifian <am.sharifian@gmail.com>
2020-11-23 20:37:50 -0800

    Add queue compatible with fpga brams

--- src/main/scala/node/DebugStore.scala
+++ src/main/scala/node/DebugStore.scala
@@ -9,7 +9,99 @@ import dandelion.shell.DMEWriteMaster
 import utility.Constants._
 import utility.UniformPrintfs
 import chipsalliance.rocketchip.config._
+import chisel3.experimental.{DataMirror, requireIsChiselType}
+import chisel3.internal.naming.chiselName
 //import tensorKernels.URAM_Queue
+
+
+
+/** A hardware module implementing a Queue
+  * @param gen The type of data to queue
+  * @param entries The max number of entries in the queue
+  * @param pipe True if a single entry queue can run at full throughput (like a pipeline). The ''ready'' signals are
+  * combinationally coupled.
+  * @param flow True if the inputs can be consumed on the same cycle (the inputs "flow" through the queue immediately).
+  * The ''valid'' signals are coupled.
+  *
+  * @example {{{
+  * val q = Module(new Queue(UInt(), 16))
+  * q.io.enq <> producer.io.out
+  * consumer.io.in <> q.io.deq
+  * }}}
+  */
+@chiselName
+class QueueFPGA[T <: Data](gen: T,
+                       val entries: Int,
+                       pipe: Boolean = false,
+                       flow: Boolean = false)
+                      (implicit compileOptions: chisel3.CompileOptions)
+  extends Module() {
+  require(entries > -1, "Queue must have non-negative number of entries")
+  require(entries != 0, "Use companion object Queue.apply for zero entries")
+  val genType = if (compileOptions.declaredTypeMustBeUnbound) {
+    requireIsChiselType(gen)
+    gen
+  } else {
+    if (DataMirror.internal.isSynthesizable(gen)) {
+      chiselTypeOf(gen)
+    } else {
+      gen
+    }
+  }
+
+  val io = IO(new QueueIO(genType, entries))
+
+  val ram = SyncReadMem(entries, genType)
+  val enq_ptr = Counter(entries)
+  val deq_ptr = Counter(entries)
+  val maybe_full = RegInit(false.B)
+
+  val ptr_match = enq_ptr.value === deq_ptr.value
+  val empty = ptr_match && !maybe_full
+  val full = ptr_match && maybe_full
+  val do_enq = WireDefault(io.enq.fire())
+  val do_deq = WireDefault(io.deq.fire())
+
+  when (do_enq) {
+    ram(enq_ptr.value) := io.enq.bits
+    enq_ptr.inc()
+  }
+  when (do_deq) {
+    deq_ptr.inc()
+  }
+  when (do_enq =/= do_deq) {
+    maybe_full := do_enq
+  }
+
+  io.deq.valid := !empty
+  io.enq.ready := !full
+  io.deq.bits := ram(deq_ptr.value)
+
+  if (flow) {
+    when (io.enq.valid) { io.deq.valid := true.B }
+    when (empty) {
+      io.deq.bits := io.enq.bits
+      do_deq := false.B
+      when (io.deq.ready) { do_enq := false.B }
+    }
+  }
+
+  if (pipe) {
+    when (io.deq.ready) { io.enq.ready := true.B }
+  }
+
+  val ptr_diff = enq_ptr.value - deq_ptr.value
+  if (isPow2(entries)) {
+    io.count := Mux(maybe_full && ptr_match, entries.U, 0.U) | ptr_diff
+  } else {
+    io.count := Mux(ptr_match,
+      Mux(maybe_full,
+        entries.asUInt, 0.U),
+      Mux(deq_ptr.value > enq_ptr.value,
+        entries.asUInt + ptr_diff, ptr_diff))
+  }
+}
+
 
 /**
  * @brief Store Node. Implements store operations
@@ -42,7 +134,7 @@ class DebugBufferNode(
   val dbg_counter = Counter(1024)
 
   //Is the Data The Wires of the Boring Connection Will put data in.
-  val LogData = Module(new Queue(UInt((xlen).W), 20))
+  val LogData = Module(new QueueFPGA(UInt((xlen).W), 20))
 
 
   val st_node = Module(new UnTypDebugStore(ID = 0, RouteID = RouteID))
